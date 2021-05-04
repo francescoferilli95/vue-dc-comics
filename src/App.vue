dbabcf30afdf9e31bda200d08fc187f024357bbb
francescoferilli95 <francesco.ferilli1995@libero.it>
2021-05-04 15:52:45 +0200

    creating scaffolding

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- HEADER -->
+    <header>
+      <Header />
+    </header>
+    
+    <!-- MAIN CONTENT -->
+    <main>
+
+    </main>
+
+    <!-- FOOTER -->
+    <footer>
+
+    </footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from '@/components/Header.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style>
+
+  /* GENERAL */
+  .container {
+    width: 1000px;
+    margin: 0 auto;
+  }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,48 @@
+<template>
+    <header>
+        <div class="container">
+            <div class="logo">
+                <img src="../assets/img/dc-logo.png" alt="DC LOGO HEADER">
+            </div>
+            <nav class="navigation">
+                <ul>
+                    <li><a href="/">Characters</a></li>
+                    <li><a href="/">Characters</a></li>
+                    <li><a href="/">Characters</a></li>
+                    <li><a href="/">Characters</a></li>
+                    <li><a href="/">Characters</a></li>
+                    <li><a href="/">Characters</a></li>
+                    <li><a href="/">Characters</a></li>
+                    <li><a href="/">Characters</a></li>
+                    <li><a href="/">Characters</a></li>
+                    <li><a href="/">Characters</a></li>
+
+                </ul>
+            </nav>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+}
+</script>
+
+<style scoped>
+.container {
+    display: flex;
+    justify-content: space-between;
+}
+
+.navigation ul {
+    display: flex;
+    list-style: none;
+}
+
+ul a {
+    padding: 1rem;
+    text-decoration: none;
+    color: #000;
+}
+</style>
