7ec9724277748db44a467d3f10cd7b2e47a48c69
francescoferilli95 <francesco.ferilli1995@libero.it>
2021-05-05 15:56:14 +0200

    adding more styles

--- src/components/Comics.vue
+++ src/components/Comics.vue
@@ -17,12 +17,14 @@ export default {
 .box {
     flex-basis: calc(100% / 6);
     img {
-        width: 50%;
+        width: 70%;
         margin-top: 5px;
         margin-bottom: 5px;
     }
     p {
         margin-bottom: 20px;
+        text-transform: uppercase;
+        font-size: 14px
     }
 }
 
